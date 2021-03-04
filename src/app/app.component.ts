ccf7da86ba5bbc80ef06394ef310e6691122155a
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2021-03-04 03:22:19 +0300

    feat: add title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'bookShop';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('appTitle') public appTitle!: ElementRef;
+
+  ngAfterViewInit() {
+    this.appTitle.nativeElement.textContent = 'Book Shop'
+  }
+
 }
