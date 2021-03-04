2f9c9d93f3f6c5970dc82db578e964eea592681b
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2021-03-04 15:04:16 +0200

    feat: add enum for routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
+import { RoutingPaths } from './core/enums/routing-paths.enum';
 import { CartComponent } from './components/cart/cart.component';
 import { BookComponent } from './components/book/book.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: BookComponent },
-  { path: 'cart', component: CartComponent }
+  { path: RoutingPaths.bookPage, component: BookComponent },
+  { path: RoutingPaths.cartPage, component: CartComponent }
 ];
 
 @NgModule({
