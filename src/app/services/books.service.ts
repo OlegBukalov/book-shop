1ef6093a9e713cf959530de2fc60fa73b30681de
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2021-03-16 22:47:48 +0300

    fix: fix type any

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,3 +1,4 @@
+import { IBook } from './../models/book.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -9,7 +10,7 @@ export class BooksService {
 
   constructor(private http: HttpClient) { }
 
-  public getBooks(): Observable<any> {
-    return this.http.get('assets/books.json');
+  public getBooks(): Observable<IBook[]> {
+    return this.http.get<IBook[]>('assets/books.json');
   }
 }
