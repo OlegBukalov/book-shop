378e5143355f3ee3af0a49d8aa02703892869d89
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2021-03-17 15:52:22 +0200

    fix

--- src/app/shared/pipes/order-by.pipe.ts
+++ src/app/shared/pipes/order-by.pipe.ts
@@ -8,20 +8,12 @@ export class OrderByPipe implements PipeTransform {
 
   transform(products: IProduct[], field: string, sortFlag: boolean = false): IProduct[] {
     if (sortFlag) {
-      if (field === 'name') {
-        products.sort((a, b) => a.name > b.name ? -1 : 1);
-      } else if (field === 'amount') {
-        products.sort((a, b) => a.amount - b.amount);
-      } else if (field === 'price') {
-        products.sort((a, b) => a.priceSum - b.priceSum);
+      if (field === 'name' || field === 'amount' || field === 'price') {
+        products.sort((a, b) => a[field] > b[field] ? 1 : -1);
       }
     } else {
-      if (field === 'name') {
-        products.sort((a, b) => a.name > b.name ? 1 : -1);
-      } else if (field === 'amount') {
-        products.sort((a, b) => b.amount - a.amount);
-      } else if (field === 'price') {
-        products.sort((a, b) => b.priceSum - a.priceSum);
+      if (field === 'name' || field === 'amount' || field === 'price') {
+        products.sort((a, b) => a[field] > b[field] ? -1 : 1);
       }
     }
     return products;
